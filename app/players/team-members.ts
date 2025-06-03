fe36e92592140930d79a1fb598d835b51f98e537
posij118 <schuttka@seznam.cz>
2025-06-03 13:25:37 +0100

    Add team members

---
+++ app/players/team-members.ts
@@ -0,0 +1,14 @@
+export const currentTeamMembers = [
+    "Pavel Hudec",
+    "Martin Mojžíš",
+    "Martin Čeliňák",
+    "Jakub Hertl",
+    "Martin Boreš",
+    "Petra Vidnerová",
+    "Pavel Raus",
+];
+
+export const formerTeamMembers = [];
+
+export const teamCaptain = "Pavel Hudec";
+export const formerTeamCaptain = "Martin Čeliňák";
