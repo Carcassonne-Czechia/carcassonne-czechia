f1c2e4c3210b347f6d77be9e4df768808d295c50
posij118 <schuttka@seznam.cz>
2025-06-06 13:26:09 +0100

    Add global ignores to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
@@ -36,4 +36,5 @@ export default defineConfig([
             eqeqeq: ["error", "smart"],
         },
     },
+    globalIgnores(["build/**", ".react-router/**"]),
 ]);
