6a86f07d202d60b7115360aaa5ea2073a0a28e74
posij118 <schuttka@seznam.cz>
2025-06-07 13:14:39 +0100

    Add bios bundler

---
+++ scripts/bios-bundler.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+src_base_path = (
+    os.path.join("src", "bios")
+    if os.getenv("CI")
+    else os.path.join("..", "src", "bios")
+)
+
+all_bios_path = (
+    os.path.join("public", "all-bios.json")
+    if os.getenv("CI")
+    else os.path.join("..", "public", "all-bios.json")
+)
+
+obj: dict[str, dict[str, str]] = {}
+
+for f in os.listdir(src_base_path):
+    base, _ = f.split(".")
+    parts = base.split("-")
+    bga_username = "-".join(parts[:-1])
+    lang = parts[-1]
+
+    with open(os.path.join(src_base_path, f), "r") as file:
+        raw_markdown = file.read()
+        obj.setdefault(bga_username, {}).setdefault(lang, raw_markdown)
+
+obj_str = json.dumps(obj)
+if os.path.exists(all_bios_path): os.remove(all_bios_path)
+with open(all_bios_path, "x") as file:
+    file.write(obj_str)
