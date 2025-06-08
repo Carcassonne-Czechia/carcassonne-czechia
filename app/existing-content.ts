e93a9123e2ac9a2913443f1abbcf8dd8f1dd07f4
posij118 <schuttka@seznam.cz>
2025-06-08 20:27:23 +0100

    Remove existing bios variable

--- app/existing-content.ts
+++ app/existing-content.ts
@@ -2,12 +2,6 @@ import type {
     CurrentTeamMemberBGAUsername,
     FormerTeamMemberBGAUsername,
 } from "~/players/team-members";
-import type { Lang } from "./i18n/lang-context";
-
-type BioMetadata = {
-    BGA_Username: CurrentTeamMemberBGAUsername | FormerTeamMemberBGAUsername;
-    langs: Lang[];
-};
 
 export const HAS_AVATAR: (
     | CurrentTeamMemberBGAUsername
