98af35fbaf07d9f26ae58b083306ebe58f0b125c
posij118 <schuttka@seznam.cz>
2025-06-08 20:26:26 +0100

    Remove existing bios variable

--- app/existing-content.ts
+++ app/existing-content.ts
@@ -9,13 +9,6 @@ type BioMetadata = {
     langs: Lang[];
 };
 
-export const EXISTING_BIOS: BioMetadata[] = [
-    {
-        BGA_Username: "posij118",
-        langs: ["en"],
-    },
-] as const;
-
 export const HAS_AVATAR: (
     | CurrentTeamMemberBGAUsername
     | FormerTeamMemberBGAUsername
